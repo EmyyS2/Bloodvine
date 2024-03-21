4c06c8b47d46d6d3664478e049b95d884a8c1797
EmyyS2 <miacontapcsenai@gmail.com>
2024-03-21 15:47:23 -0300

    login de cliente

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HappyMeal from './src/HappyMeal';
 import CarrinhoHappyMeal from './src/CarrinhoHappyMeat';
+import LoginHappyMeal from './src/assets/LoginHappyMeal';
 function App():React.JSX.Element{
   return(
-    <CarrinhoHappyMeal/>
+    //<CarrinhoHappyMeal/>
  //<HappyMeal/>
+ <LoginHappyMeal/>
   );
 }
 
